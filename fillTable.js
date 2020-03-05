9b55cee8f4b8440ccf5d61cab21a6c01cb956a4b
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2020-03-05 16:06:56 -0600

    JSON parsing works

--- fillTable.js
+++ fillTable.js
@@ -1,28 +1,21 @@
 function retrieveData(){
     var indexInTable = 1;   //will have table headers
-    var JSONData = getDataFromJSON('data.json');
-
+    var location = "json.data";
     var table = document.getElementById("dataTable");
-    var row = table.insertRow(indexInTable);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
+
+    $.getJSON("data.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+
+        for(i=0;i<json.count;i++){
+            var row = table.insertRow(indexInTable);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.innerHTML = "NEW CELL1";
+            cell2.innerHTML = "NEW CELL2";
+        }
+    });
 }
 
-//asynchronously loads JSON data
-//https://stackoverflow.com/questions/16991341/json-parse-file-path
-function getDataFromJSON(location){
-    var request = new XMLHttpRequest();
-    request.open("GET", location, false);
-    request.send(null);
-    request.onreadystatechange = function() {
-      if ( request.readyState === 4 && request.status === 200 ) {
-        var my_JSON_object = JSON.parse(request.responseText);
-        return(my_JSON_object);
-      }
-    }
-}
 window.onload=function(){
     document.getElementById('retrieveButton').addEventListener('click',retrieveData);
 }
