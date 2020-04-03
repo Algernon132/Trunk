3ba36c4c48e11cc5882eb762e741eaa16f869290
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2020-04-03 14:09:23 -0500

    JSON retrieval has been reworked, seems to be functional

--- retrieveTableData.js
+++ retrieveTableData.js
@@ -1,9 +1,8 @@
 function retrieveData(){
-    //id of table to be filled
-    var table = "dataTable";
+    var table = "dataTable";                //id of table to be filled
     var location = "data.json";
-    var data=null;
-    //data will be sent to the server. Can use this for user authentication
+    var data=null;                          //data will be sent to the server. Can use this for user authentication
+    
     //shorthand for ajax call (ASYNC)
     $.getJSON(location, data,
         function (data, textStatus, jqXHR) {
@@ -11,6 +10,7 @@ function retrieveData(){
            if(textStatus == "success"){
             const userData = new UserData(data);
             userData.fillTable(table);
+            userData.printToConsole();
            }else{
                console.log("An error occured.");
                console.log(textStatus);
--- userData.js
+++ userData.js
@@ -2,36 +2,40 @@ class UserData{
     constructor(jsonData){
         var i;
         //domain,username,password are arrays
-        this.numberofRows = jsonData.count;
-        this.domain = [];
+        this.numberOfRows = jsonData.count;
+        //this.parsedData = JSON.parse(jsonData);
+        this.domain=[];
         this.username=[];
         this.password=[];
-        for(i=0;i++;i<=count){
-            domain.push(jsonData.data[i].domain);
-            username.push(jsonData.data[i].username);
-            password.push(jsonData.data[i].password);
+        for(i=0;i<this.numberOfRows;i++){
+            console.log(JSON.stringify(jsonData.data[i]));
+            this.domain.push(jsonData.data[i].domain);
+            this.username.push(jsonData.data[i].username);
+            this.password.push(jsonData.data[i].password);
         }
-        console.log(this.numberofRows);
 
     }
 
    fillTable(tableID){
        var table=document.getElementById(tableID);
-       console.log(this.numberofRows);
        var i = 0;
        var indexInTable = 1;    //headers have index 0
-    for(i=0;i<this.numberOfRows;i++){
-        var row = table.insertRow(indexInTable++);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        cell1.innerHTML = this.domain[i];
-        cell2.innerHTML = this.username[i];
-        cell3.innerHTML = this.password[i];
-    }
-
+      for(i=0;i<this.numberOfRows;i++){
+             var row = table.insertRow(indexInTable++);
+            var cell1 = row.insertCell(0);
+             var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            cell1.innerHTML = this.domain[i];
+            cell2.innerHTML = this.username[i];
+            cell3.innerHTML = this.password[i];
+        }
    }
 
+   printToConsole(){
+       console.log(JSON.stringify(this.domain));
+       console.log(JSON.stringify(this.username));
+       console.log(JSON.stringify(this.domain));
+   }
    
 
 }
