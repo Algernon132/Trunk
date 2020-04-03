4958b49efc763f595e20642e6929adf459d2b2df
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2020-04-02 19:26:08 -0500

    Data retrieval works, not displaying table though

--- retrieveTableData.js
+++ retrieveTableData.js
@@ -1,11 +1,26 @@
 function retrieveData(){
-    var location = "json.data";
+    //id of table to be filled
+    var table = "dataTable";
+    var location = "data.json";
     var data=null;
     //data will be sent to the server. Can use this for user authentication
-    $.$.getJSON(location, data,
+    //shorthand for ajax call (ASYNC)
+    $.getJSON(location, data,
         function (data, textStatus, jqXHR) {
-           
+            console.log("request fired");
+           if(textStatus == "success"){
+            const userData = new UserData(data);
+            userData.fillTable(table);
+           }else{
+               console.log("An error occured.");
+               console.log(textStatus);
+           }
         }
-    );
+    );  //end getJSON
+}
+
+window.onload=function(){
+    console.log("onload fired");
+    this.retrieveData();
 }
 
--- userData.js
+++ userData.js
@@ -1,26 +1,37 @@
 class UserData{
-    constructor(numberOfRows){
+    constructor(jsonData){
+        var i;
+        //domain,username,password are arrays
+        this.numberofRows = jsonData.count;
+        this.domain = [];
+        this.username=[];
+        this.password=[];
+        for(i=0;i++;i<=count){
+            domain.push(jsonData.data[i].domain);
+            username.push(jsonData.data[i].username);
+            password.push(jsonData.data[i].password);
+        }
+        console.log(this.numberofRows);
 
     }
 
-    get status(){
-
+   fillTable(tableID){
+       var table=document.getElementById(tableID);
+       console.log(this.numberofRows);
+       var i = 0;
+       var indexInTable = 1;    //headers have index 0
+    for(i=0;i<this.numberOfRows;i++){
+        var row = table.insertRow(indexInTable++);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = this.domain[i];
+        cell2.innerHTML = this.username[i];
+        cell3.innerHTML = this.password[i];
     }
 
-    get numberOfRows(){
-
    }
 
+   
 
-   get domain(index){
-
-   }
-
-   get username(index){
-
-   }
-
-   get password(index){
-       
-   }
 }
