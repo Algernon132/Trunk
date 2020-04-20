8b427fd17b944ad26741c8d5de7db42e422518c8
kylestevenson <krs6y9@mail.missouri.edu>
2020-04-20 17:47:00 -0500

    signup page link added

--- trunk-app/src/app/app-routing.module.ts
+++ trunk-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UserConsoleComponent } from './user-console/user-console.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'user', component: UserConsoleComponent }
+  { path: 'user', component: UserConsoleComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
