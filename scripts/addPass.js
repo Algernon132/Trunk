d1650d6ed11d4e26ef72ad5559c78143f932eeba
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2020-05-09 17:45:19 -0500

    cleaned up

--- scripts/addPass.js
+++ scripts/addPass.js
@@ -1,23 +1,35 @@
-// function addPass(nameField,urlField,usernameField,passwordField){
-//     var serverAddress = "http://ec2-3-21-190-112.us-east-2.compute.amazonaws.com:8080/users/addAcc";
-//      $.post(serverAddress,
-//          ({userID: "5eab2f96d4b18c04c7acd561",
-//          name: nameField,
-//          url: urlField,
-//          accUsername: usernameField,
-//          accPassword: passwordField})
-//         )
-//      .done(function(data){
-//          console.log("data: " + JSON.stringify(data));
-//          window.alert(JSON.stringify(data));
-//          })
-//      .fail(function(error){
-//          window.alert("Error adding password: " + JSON.stringify(error));
-//          })
-//      .always(function(){
-//          console.log("always");
-//      });
-//     }//end addPass
+function addPass(nameField,urlField,usernameField,passwordField){
+    var serverAddress = "http://ec2-3-21-190-112.us-east-2.compute.amazonaws.com:8080/users/addAcc";
+
+    chrome.storage.sync.get("userID", function(result) {
+        if('userID' in result && result.userID != null){
+            $.post(serverAddress,
+                    ({userID: result.userID,
+                    name: nameField,
+                    url: urlField,
+                    accUsername: usernameField,
+                    accPassword: passwordField})
+                    )
+                .done(function(data){
+                    console.log("data: " + JSON.stringify(data));
+                    window.alert(JSON.stringify(data));
+                    })
+                .fail(function(error){
+                    window.alert("Error adding password: " + JSON.stringify(error));
+                    })
+                .always(function(){
+                    console.log("always");
+                });
+        }else{
+            console.log("Failed to retrieve credentials");
+            return null;
+        }
+      });
+
+
+
+     
+    }//end addPass
 
 window.onload=function(){
     $("#submitPassButton").click(function(){
@@ -30,21 +42,21 @@ window.onload=function(){
     });
 };
 
-function addPass(nameField,urlField,usernameField,passwordField){
-    var xmlhttp = new XMLHttpRequest();
-    var theUrl = "http://ec2-3-21-190-112.us-east-2.compute.amazonaws.com:8080/users/addAcc";
-    xmlhttp.onreadystatechange = function() {
-        console.log("xmlhttp status: " + this.status);
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.responseText);
-        }
-      };
-    xmlhttp.open("POST", theUrl);
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xmlhttp.send(JSON.stringify({
-            userID: "5eab2f96d4b18c04c7acd561",
-             name: nameField,
-             url: urlField,
-             accUsername: usernameField,
-             accPassword: passwordField}));
-}
+// function addPass(nameField,urlField,usernameField,passwordField){
+//     var xmlhttp = new XMLHttpRequest();
+//     var theUrl = "http://ec2-3-21-190-112.us-east-2.compute.amazonaws.com:8080/users/addAcc";
+//     xmlhttp.onreadystatechange = function() {
+//         console.log("xmlhttp status: " + this.status);
+//         if (this.readyState == 4 && this.status == 200) {
+//           console.log(this.responseText);
+//         }
+//       };
+//     xmlhttp.open("POST", theUrl);
+//     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//     xmlhttp.send(JSON.stringify({
+//             userID: "5eab2f96d4b18c04c7acd561",
+//              name: nameField,
+//              url: urlField,
+//              accUsername: usernameField,
+//              accPassword: passwordField}));
+// }
--- scripts/userData.js
+++ scripts/userData.js
@@ -1,9 +1,12 @@
 //class for holding and retrieving user data. A single object of class UserData can hold and display all user data.
-
+//fillTable and pritnToConsole will fail if fillData() isn't run first.
 class UserData{
-    constructor(jsonData){
+    constructor(){
+    }
+
+    fillData(jsonData){
         var i;
-        console.log(JSON.stringify(jsonData));
+        //console.log(JSON.stringify(jsonData));
         //domain,username,password are arrays
         this.numberOfRows = jsonData.length;
         //this.parsedData = JSON.parse(jsonData);
@@ -20,21 +23,29 @@ class UserData{
     }
 
    fillTable(tableID){
+       if(typeof this.numberOfRows == 'undefined'){
+           return;
+       }
        var table=document.getElementById(tableID);
        var i = 0;
        var indexInTable = 1;    //headers have index 0
       for(i=0;i<this.numberOfRows;i++){
-             var row = table.insertRow(indexInTable++);
+            var row = table.insertRow(indexInTable++);
             var cell1 = row.insertCell(0);
-             var cell2 = row.insertCell(1);
+            var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
             cell1.innerHTML = this.domain[i];
             cell2.innerHTML = this.username[i];
             cell3.innerHTML = this.password[i];
+            $(cell3).addClass("password");
+            
         }
    }
 
    printToConsole(){
+    if(typeof this.numberOfRows == 'undefined'){
+        return;
+    }
        console.log(JSON.stringify(this.domain));
        console.log(JSON.stringify(this.username));
        console.log(JSON.stringify(this.password));
